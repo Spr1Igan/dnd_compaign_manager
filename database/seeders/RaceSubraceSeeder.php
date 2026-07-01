6a0c9b1b9c3a6203fe326f24481dbafca1808ce1
Spr1gan <8leon80@gmail.com>
2026-07-01 20:39:40 +0300

    v0.13.0

--- database/seeders/RaceSubraceSeeder.php
+++ database/seeders/RaceSubraceSeeder.php
@@ -24,11 +24,27 @@ class RaceSubraceSeeder extends Seeder
                 ['name' => 'Lightfoot Halfling', 'slug' => 'lightfoot-halfling', 'ability_bonuses' => ['charisma' => 1], 'features' => ['naturally-stealthy']],
                 ['name' => 'Stout Halfling', 'slug' => 'stout-halfling', 'ability_bonuses' => ['constitution' => 1], 'features' => ['stout-resilience']],
             ],
+            'dragonborn' => [
+                ['name' => 'Black Dragon Ancestry', 'slug' => 'black-dragonborn', 'features' => ['black-dragon-ancestry', 'breath-weapon-acid-line', 'damage-resistance-acid']],
+                ['name' => 'Blue Dragon Ancestry', 'slug' => 'blue-dragonborn', 'features' => ['blue-dragon-ancestry', 'breath-weapon-lightning-line', 'damage-resistance-lightning']],
+                ['name' => 'Brass Dragon Ancestry', 'slug' => 'brass-dragonborn', 'features' => ['brass-dragon-ancestry', 'breath-weapon-fire-line', 'damage-resistance-fire']],
+                ['name' => 'Bronze Dragon Ancestry', 'slug' => 'bronze-dragonborn', 'features' => ['bronze-dragon-ancestry', 'breath-weapon-lightning-line', 'damage-resistance-lightning']],
+                ['name' => 'Copper Dragon Ancestry', 'slug' => 'copper-dragonborn', 'features' => ['copper-dragon-ancestry', 'breath-weapon-acid-line', 'damage-resistance-acid']],
+                ['name' => 'Gold Dragon Ancestry', 'slug' => 'gold-dragonborn', 'features' => ['gold-dragon-ancestry', 'breath-weapon-fire-cone', 'damage-resistance-fire']],
+                ['name' => 'Green Dragon Ancestry', 'slug' => 'green-dragonborn', 'features' => ['green-dragon-ancestry', 'breath-weapon-poison-cone', 'damage-resistance-poison']],
+                ['name' => 'Red Dragon Ancestry', 'slug' => 'red-dragonborn', 'features' => ['red-dragon-ancestry', 'breath-weapon-fire-cone', 'damage-resistance-fire']],
+                ['name' => 'Silver Dragon Ancestry', 'slug' => 'silver-dragonborn', 'features' => ['silver-dragon-ancestry', 'breath-weapon-cold-cone', 'damage-resistance-cold']],
+                ['name' => 'White Dragon Ancestry', 'slug' => 'white-dragonborn', 'features' => ['white-dragon-ancestry', 'breath-weapon-cold-cone', 'damage-resistance-cold']],
+            ],
             'gnome' => [
                 ['name' => 'Forest Gnome', 'slug' => 'forest-gnome', 'ability_bonuses' => ['dexterity' => 1], 'features' => ['natural-illusionist', 'speak-with-small-beasts']],
                 ['name' => 'Rock Gnome', 'slug' => 'rock-gnome', 'ability_bonuses' => ['constitution' => 1], 'features' => ['artificers-lore', 'tinker']],
             ],
         ];
+
+        RaceSubrace::query()
+            ->where('slug', 'dragonborn-traits')
+            ->delete();
 
         foreach ($subraces as $raceSlug => $items) {
             $race = Race::query()->where('slug', $raceSlug)->first();
